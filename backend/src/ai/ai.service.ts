b462a942aa62ce931c6ad3e903b6929196819f88
Rohit Gite <rohitgite1419@gmail.com>
2025-12-22 23:19:46 +0530

    Add AiModule and AiService for Google GenAI vector embeddings

---
+++ backend/src/ai/ai.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { GoogleGenAI } from '@google/genai';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AiService {
+  private genAI: GoogleGenAI;
+  constructor(private config: ConfigService) {
+    this.genAI = new GoogleGenAI({
+      apiKey: this.config.get('GOOGLE_API_KEY'),
+    });
+  }
+  // normalising the embedding
+  private normalize(vector: number[]): number[] {
+    const magnitude = Math.sqrt(
+      vector.reduce((sum, val) => sum + val * val, 0),
+    );
+    if (magnitude === 0) {
+      throw new Error('Cannot normalize a zero vector');
+    }
+    return vector.map((val) => val / magnitude);
+  }
+
+  //    generating the embedding for the given content
+  async gnerateEmbedding(text: string): Promise<number[]> {
+    const response = await this.genAI.models.embedContent({
+      model: 'gemini-embedding-001',
+      contents: [text],
+      config: {
+        outputDimensionality: 768,
+      },
+    });
+
+    const embeddings = response.embeddings;
+
+    if (!embeddings || embeddings.length === 0 || !embeddings[0].values) {
+      throw new Error('Failed to generate embedding');
+    }
+
+    return this.normalize(embeddings[0].values);
+  }
+}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { AiModule } from './ai/ai.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AiModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
