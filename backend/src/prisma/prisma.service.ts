ccb3e7739ef19f995be9c89f80e83cc412ef6342
Rohit Gite <rohitgite1419@gmail.com>
2025-12-22 19:36:04 +0530

    added prisma client

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Global, Injectable } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
+
+@Global()
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    const connectionString = config.get('DATABASE_URL')!;
+    const adapter = new PrismaPg({ connectionString });
+
+    console.log('db connecting to', connectionString);
+
+    super({ adapter });
+  }
+}
