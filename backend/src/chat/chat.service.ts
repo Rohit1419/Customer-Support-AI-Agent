6526bd5ff28aeb95751e0568d9baba0fba992eeb
Rohit Gite <rohitgite1419@gmail.com>
2025-12-24 02:09:42 +0530

    Implement getChatResponse method in ChatService for handling user queries

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -1,5 +1,70 @@
 import { Injectable } from '@nestjs/common';
-import { GoogleGenAI } from '@google/genai';
+import { AiService } from '../ai/ai.service';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(
+    private aiService: AiService,
+    private prisma: PrismaService,
+  ) {}
+
+  // get the chat
+
+  async getChatResponse(userPrompt: string, sessionId: string) {
+    // geneare user query vector
+
+    const userQueryVector = await this.aiService.generateEmbedding(userPrompt);
+
+    // getting the relevent vector from teh pgVector
+
+    const context: any[] = await this.prisma.$queryRawUnsafe(
+      `
+      SELECT content
+      FROM "Knowlagebase"
+      ORDER BY 1 - (embedding <=> $1::vector) DESC
+      LIMIT 3
+    `,
+      `[${userQueryVector.join(',')}]`,
+    );
+    const contextText = context.map((c) => c.content).join('\n\n');
+
+    const systemPrompt = `You are a helpful Spur support agent.
+        Use the following context to answer questions.
+        If unsure, say you don't know.
+
+        Context:
+        ${contextText}`;
+
+    // now chatting withh LLLM to get the response
+    const aiResponse = await this.aiService.generateChatResponse({
+      systemPrompt,
+      userPrompt,
+    });
+
+    // updating the conversaton history
+    await this.prisma.conversation.upsert({
+      where: { id: sessionId },
+      update: {
+        messages: {
+          create: [
+            { sender: 'user', text: userPrompt },
+            { sender: 'ai', text: aiResponse },
+          ],
+        },
+      },
+      create: {
+        id: sessionId,
+        messages: {
+          create: [
+            { sender: 'user', text: userPrompt },
+            { sender: 'ai', text: aiResponse },
+          ],
+        },
+      },
+    });
+
+    return { response: aiResponse };
+  }
+}
