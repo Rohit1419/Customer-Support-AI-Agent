619e7c79f38ee7ced9f5f0b837a9d771352b5a79
Rohit Gite <rohitgite1419@gmail.com>
2025-12-26 18:00:48 +0530

    feat: add markdown file ingestion with chunk processing and embedding generation

--- backend/src/knowledge-base/knowledge-base.service.ts
+++ backend/src/knowledge-base/knowledge-base.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AiService } from 'src/ai/ai.service';
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
+import matter from 'gray-matter';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class KnowledgeBaseService {
@@ -9,14 +13,51 @@ export class KnowledgeBaseService {
     private aiService: AiService,
   ) {}
 
-  async ingestData(content: string, category: string) {
-    const vector = await this.aiService.generateEmbedding(content);
+  async ingestMarkDownFile(filePath: string) {
+    if (!fs.existsSync(filePath)) {
+      throw new Error(`File not found: ${filePath}`);
+    }
 
-    return this.prisma.$executeRawUnsafe(
-      `INSERT INTO "KnowlageBase" (id, content, category, embedding) VALUES (gen_random_uuid(), $1, $2, $3::vector)`,
-      content,
-      category,
-      `[${vector.join(',')}]`,
+    if (path.extname(filePath) !== '.md') {
+      throw new Error(`Not a markdown file: ${filePath}`);
+    }
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const { data: metadata, content: rawFileContent } = matter(fileContent);
+
+    const textsplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 800,
+      chunkOverlap: 100,
+    });
+
+    const chunks = await textsplitter.createDocuments([rawFileContent]);
+    const chunkContents = chunks.map((chunk) => chunk.pageContent);
+
+    // generate embedding
+    const vectors = await this.aiService.generateEmbedding(chunkContents);
+
+    // batch insert
+    for (let i = 0; i < chunkContents.length; i++) {
+      const content = chunkContents[i];
+      const vectorStr = `[${vectors[i].join(',')}]`;
+
+      await this.prisma.$executeRaw`
+      INSERT INTO "KnowledgeChunk"
+      (id, content, "sourceFile", "sourceType", priority, embedding, "createdAt", "updatedAt")
+      VALUES (
+        gen_random_uuid(),
+        ${content},
+        ${filePath},
+        ${metadata.category ?? 'general'},
+        ${metadata.priority ?? 0},
+        ${vectorStr}::vector,
+        NOW(),
+        NOW()
+      )
+    `;
+    }
+
+    console.log(
+      `Processed and stored chunks from file: ${filePath} and created (${chunkContents.length} chunks)`,
     );
   }
 }
